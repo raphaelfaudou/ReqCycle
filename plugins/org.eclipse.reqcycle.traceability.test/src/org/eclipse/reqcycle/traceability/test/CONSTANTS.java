280d158fcbd4aff7dd6a012a4ebb82bdf7eb26c4
Anass RADOUANI <anass.radouani@atos.net>
2013-06-04 19:43:03 +0200

    Remove unused plugins

--- plugins/org.eclipse.reqcycle.traceability.test/src/org/eclipse/reqcycle/traceability/test/CONSTANTS.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.reqcycle.traceability.test;
-
-
-public interface CONSTANTS {
-	public static String RESOURCES = "/resources/" ;
-	public static String RESOURCE_FULL_PATH = Activator.PLUGIN_ID + "/resources/" ;
-}
