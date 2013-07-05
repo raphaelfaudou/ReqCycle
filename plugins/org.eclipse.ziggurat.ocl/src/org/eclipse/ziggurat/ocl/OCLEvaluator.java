1332584c85c1a905c8ae577186f41aaf5c802991
omelois <olivier.melois@atos.net>
2013-07-05 11:13:31 +0200

    First commit of the ziggurat.ocl project.

---
+++ plugins/org.eclipse.ziggurat.ocl/src/org/eclipse/ziggurat/ocl/OCLEvaluator.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Atos.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Atos - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ziggurat.ocl;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.ocl.ParserException;
+import org.eclipse.ziggurat.ocl.extensions.JavaImplementedOCLOperation;
+
+/**
+ * An OCL evaluator that can evaluate OCL operations and queries by passing it 
+ * strings representing the OCL code.
+ */
+public interface OCLEvaluator {
+
+	/**
+	 * Compiles an operation defined in ocl, in the OCL environment. The context on which the operation is applicable
+	 * is computed from the metamodel prefix and the classifier name.
+	 */
+	public EOperation compileOperation(String operationDef, String metaModelPrefix, String eClassifierContext) throws ParserException;
+	
+	
+	/**
+	 * Compiles an operation defined in ocl, in the OCL environment.
+	 */
+	public EOperation compileOperation(String operationDef, EClassifier eClassifierContext) throws ParserException;
+	
+	/**
+	 * Compiles an operation defined in Java, in the OCL environment.
+	 * @param operation
+	 * @return
+	 */
+	public EOperation compileOperation(JavaImplementedOCLOperation operation);
+
+	/**
+	 * Finds an operation that was compiled in the OCL environment of this evaluator, and that 
+	 * is compatible with the given context.
+	 */
+	public EOperation getCompiledOperation(String operationName, Object context);
+	
+	/**
+	 * Evaluates an operation on a given context, with arguments that should match the operation's parameter.
+	 */
+	public Object evaluateOperation(EOperation eOperation, EObject context, Object[] args);
+
+	/**
+	 * Evaluates a query on a given context. 
+	 */
+	public Object evaluateQuery(String queryBody, Object context) throws ParserException; 
+
+	/**
+	 * Evaluates a query on a given context, with global variables that will be erased after the evaluation of the query. 
+	 */
+	public Object evaluateQuery(String queryBody, Object context, Map<String, ? extends Object> globalVars) throws ParserException;
+	
+	
+	/**
+	 * Computes a context classifier from a metamodel prefix and a context name.
+	 */
+	public EClassifier lookupEClassifier(String metamodelPrefix, String eClassifierContext);
+	
+	/**
+	 * Sets the extent map of the OCL environment, responsible for the result of the "allInstances" operation execution.
+	 */
+	public void setOclExtentMap(Map<EClass, ? extends Set<? extends EObject>> extentMap);
+	
+	/**
+	 * Retrieves the extent map of the OCL environment.
+	 */
+	public Map<EClass, ? extends Set<? extends EObject>> getOclExtentMap();
+}
---
+++ plugins/org.eclipse.ziggurat.ocl/src/org/eclipse/ziggurat/ocl/ZigguratOCLPlugin.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Atos.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Atos - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ziggurat.ocl;
+
+import org.eclipse.ziggurat.ocl.impl.OCLEvaluatorImpl;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class ZigguratOCLPlugin implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		ZigguratOCLPlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		ZigguratOCLPlugin.context = null;
+	}
+	
+	public OCLEvaluator createOCLEvaluator(){
+		return new OCLEvaluatorImpl();
+	}
+
+}
