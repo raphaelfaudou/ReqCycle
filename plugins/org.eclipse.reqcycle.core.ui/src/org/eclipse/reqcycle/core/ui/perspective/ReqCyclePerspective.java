24146f4c1e849c2158965093b47642061bc40e81
Anass RADOUANI <anass.radouani@atos.net>
2013-08-13 11:44:19 +0200

    add check view existence on ReqCycle perspective

--- plugins/org.eclipse.reqcycle.core.ui/src/org/eclipse/reqcycle/core/ui/perspective/ReqCyclePerspective.java
+++ plugins/org.eclipse.reqcycle.core.ui/src/org/eclipse/reqcycle/core/ui/perspective/ReqCyclePerspective.java
@@ -3,37 +3,57 @@ package org.eclipse.reqcycle.core.ui.perspective;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.ErrorViewPart;
 
 
 public class ReqCyclePerspective implements IPerspectiveFactory {
+	
+	private IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 
 	/**
 	 * Creates the initial layout for a page.
 	 */
 	public void createInitialLayout(IPageLayout layout) {
-		//FIXME : Check that the views exist before adding them 
 		String editorArea = layout.getEditorArea();
 		addFastViews(layout);
 		addViewShortcuts(layout);
 		addPerspectiveShortcuts(layout);
 		{
-			IFolderLayout folderLayout = layout.createFolder("folder", IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);
-			folderLayout.addView("org.eclipse.jdt.ui.PackageExplorer");
-			
-			folderLayout.addView("org.eclipse.reqcycle.repository.ui.views.requirements");
+			IFolderLayout folderLayout = layout.createFolder("folder", IPageLayout.LEFT, 0.25f, IPageLayout.ID_EDITOR_AREA);
+			addView("org.eclipse.jdt.ui.PackageExplorer", folderLayout);
+			addView("org.eclipse.reqcycle.repository.ui.views.requirements", folderLayout);
 		}
 		{
-			IFolderLayout folderLayout = layout.createFolder("folder_2", IPageLayout.RIGHT, 0.5f, IPageLayout.ID_EDITOR_AREA);
-			folderLayout.addView("org.eclipse.reqcycle.traceability.ui.views.TraceabilityViewer");
-			folderLayout.addView("org.eclipse.reqcycle.traceability.table.partdescriptor.traceability.table");
+			IFolderLayout folderLayout = layout.createFolder("folder_2", IPageLayout.RIGHT, 0.60f, IPageLayout.ID_EDITOR_AREA);
+			addView("org.eclipse.reqcycle.traceability.ui.views.TraceabilityViewer", folderLayout);
+			addView("org.eclipse.reqcycle.traceability.table.partdescriptor.traceability.table", folderLayout);
 		}
 		{
 			IFolderLayout folderLayout = layout.createFolder("folder_1", IPageLayout.BOTTOM, 0.5f, "folder");
-			folderLayout.addView("org.eclipse.reqcycle.repository.ui.views.sources");
-			folderLayout.addView("org.eclipse.papyrus.views.modelexplorer.modelexplorer");
+			addView("org.eclipse.papyrus.views.modelexplorer.modelexplorer", folderLayout);
+			addView("org.eclipse.reqcycle.repository.ui.views.sources", folderLayout);
 		}
 	}
 
+	private void addView(String viewId, IFolderLayout folderLayout) {
+		IViewReference viewRef = activePage.findViewReference(viewId);
+		if(viewRef == null) {
+			try {
+				activePage.showView(viewId);
+				viewRef = activePage.findViewReference(viewId);
+			} catch (PartInitException e) {
+			}
+		}
+		//FIXME : check that the view exists
+		if (viewRef != null && !(viewRef.getView(false) instanceof ErrorViewPart)){
+			folderLayout.addView(viewId);
+		}
+	}
+	
 	/**
 	 * Add fast views to the perspective.
 	 */
