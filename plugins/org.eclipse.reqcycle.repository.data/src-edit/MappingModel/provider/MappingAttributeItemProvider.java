95e6a25258e8f09f48941970bc3bedec658cf2f8
Anass RADOUANI <anass.radouani@atos.net>
2013-09-19 15:29:35 +0200

    Add custom get Text for MappingAttributeItemProvider.java and MappingElementItemProvider.java

--- plugins/org.eclipse.reqcycle.repository.data/src-edit/MappingModel/provider/MappingAttributeItemProvider.java
+++ plugins/org.eclipse.reqcycle.repository.data/src-edit/MappingModel/provider/MappingAttributeItemProvider.java
@@ -3,17 +3,12 @@
 package MappingModel.provider;
 
 
-import MappingModel.MappingAttribute;
-import MappingModel.MappingModelPackage;
-
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -25,24 +20,23 @@ import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
+import MappingModel.MappingAttribute;
+import MappingModel.MappingModelPackage;
+
 /**
  * This is the item provider adapter for a {@link MappingModel.MappingAttribute} object.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
+ * 
  * @generated
  */
-public class MappingAttributeItemProvider
-	extends ItemProviderAdapter
-	implements
-		IEditingDomainItemProvider,
-		IStructuredItemContentProvider,
-		ITreeItemContentProvider,
-		IItemLabelProvider,
-		IItemPropertySource {
+public class MappingAttributeItemProvider extends ItemProviderAdapter implements IEditingDomainItemProvider, IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+
 	/**
 	 * This constructs an instance from a factory and a notifier.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public MappingAttributeItemProvider(AdapterFactory adapterFactory) {
@@ -53,11 +47,12 @@ public class MappingAttributeItemProvider
 	 * This returns the property descriptors for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-		if (itemPropertyDescriptors == null) {
+		if(itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
 			addTargetAttributePropertyDescriptor(object);
@@ -71,72 +66,40 @@ public class MappingAttributeItemProvider
 	 * This adds a property descriptor for the Target Attribute feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addTargetAttributePropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_MappingAttribute_targetAttribute_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_MappingAttribute_targetAttribute_feature", "_UI_MappingAttribute_type"),
-				 MappingModelPackage.Literals.MAPPING_ATTRIBUTE__TARGET_ATTRIBUTE,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(), getResourceLocator(), getString("_UI_MappingAttribute_targetAttribute_feature"), getString("_UI_PropertyDescriptor_description", "_UI_MappingAttribute_targetAttribute_feature", "_UI_MappingAttribute_type"), MappingModelPackage.Literals.MAPPING_ATTRIBUTE__TARGET_ATTRIBUTE, true, false, true, null, null, null));
 	}
 
 	/**
 	 * This adds a property descriptor for the Description feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addDescriptionPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_MappingAttribute_description_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_MappingAttribute_description_feature", "_UI_MappingAttribute_type"),
-				 MappingModelPackage.Literals.MAPPING_ATTRIBUTE__DESCRIPTION,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(), getResourceLocator(), getString("_UI_MappingAttribute_description_feature"), getString("_UI_PropertyDescriptor_description", "_UI_MappingAttribute_description_feature", "_UI_MappingAttribute_type"), MappingModelPackage.Literals.MAPPING_ATTRIBUTE__DESCRIPTION, true, false, false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
 	}
 
 	/**
 	 * This adds a property descriptor for the Source Id feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addSourceIdPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_MappingAttribute_sourceId_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_MappingAttribute_sourceId_feature", "_UI_MappingAttribute_type"),
-				 MappingModelPackage.Literals.MAPPING_ATTRIBUTE__SOURCE_ID,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(), getResourceLocator(), getString("_UI_MappingAttribute_sourceId_feature"), getString("_UI_PropertyDescriptor_description", "_UI_MappingAttribute_sourceId_feature", "_UI_MappingAttribute_type"), MappingModelPackage.Literals.MAPPING_ATTRIBUTE__SOURCE_ID, true, false, false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
 	}
 
 	/**
 	 * This returns MappingAttribute.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -148,14 +111,14 @@ public class MappingAttributeItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * 
+	 * @generated not
 	 */
 	@Override
 	public String getText(Object object) {
-		String label = ((MappingAttribute)object).getDescription();
-		return label == null || label.length() == 0 ?
-			getString("_UI_MappingAttribute_type") :
-			getString("_UI_MappingAttribute_type") + " " + label;
+		MappingAttribute attributeMapping = ((MappingAttribute)object);
+		String label = attributeMapping.getDescription();
+		return label == null || label.length() == 0 ? getString("_UI_AttributeMapping_type") : attributeMapping.getDescription() + " --> " + attributeMapping.getTargetAttribute().getName();
 	}
 
 	/**
@@ -163,17 +126,18 @@ public class MappingAttributeItemProvider
 	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public void notifyChanged(Notification notification) {
 		updateChildren(notification);
 
-		switch (notification.getFeatureID(MappingAttribute.class)) {
-			case MappingModelPackage.MAPPING_ATTRIBUTE__DESCRIPTION:
-			case MappingModelPackage.MAPPING_ATTRIBUTE__SOURCE_ID:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
-				return;
+		switch(notification.getFeatureID(MappingAttribute.class)) {
+		case MappingModelPackage.MAPPING_ATTRIBUTE__DESCRIPTION:
+		case MappingModelPackage.MAPPING_ATTRIBUTE__SOURCE_ID:
+			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+			return;
 		}
 		super.notifyChanged(notification);
 	}
@@ -183,6 +147,7 @@ public class MappingAttributeItemProvider
 	 * that can be created under this object.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -194,6 +159,7 @@ public class MappingAttributeItemProvider
 	 * Return the resource locator for this item provider's resources.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
--- plugins/org.eclipse.reqcycle.repository.data/src-edit/MappingModel/provider/MappingElementItemProvider.java
+++ plugins/org.eclipse.reqcycle.repository.data/src-edit/MappingModel/provider/MappingElementItemProvider.java
@@ -3,20 +3,13 @@
 package MappingModel.provider;
 
 
-import MappingModel.MappingElement;
-import MappingModel.MappingModelFactory;
-import MappingModel.MappingModelPackage;
-
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.ecore.EStructuralFeature;
-
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -28,24 +21,24 @@ import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
+import MappingModel.MappingElement;
+import MappingModel.MappingModelFactory;
+import MappingModel.MappingModelPackage;
+
 /**
  * This is the item provider adapter for a {@link MappingModel.MappingElement} object.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
+ * 
  * @generated
  */
-public class MappingElementItemProvider
-	extends ItemProviderAdapter
-	implements
-		IEditingDomainItemProvider,
-		IStructuredItemContentProvider,
-		ITreeItemContentProvider,
-		IItemLabelProvider,
-		IItemPropertySource {
+public class MappingElementItemProvider extends ItemProviderAdapter implements IEditingDomainItemProvider, IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+
 	/**
 	 * This constructs an instance from a factory and a notifier.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public MappingElementItemProvider(AdapterFactory adapterFactory) {
@@ -56,11 +49,12 @@ public class MappingElementItemProvider
 	 * This returns the property descriptors for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-		if (itemPropertyDescriptors == null) {
+		if(itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
 			addDescriptionPropertyDescriptor(object);
@@ -74,66 +68,33 @@ public class MappingElementItemProvider
 	 * This adds a property descriptor for the Description feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addDescriptionPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_MappingElement_description_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_MappingElement_description_feature", "_UI_MappingElement_type"),
-				 MappingModelPackage.Literals.MAPPING_ELEMENT__DESCRIPTION,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(), getResourceLocator(), getString("_UI_MappingElement_description_feature"), getString("_UI_PropertyDescriptor_description", "_UI_MappingElement_description_feature", "_UI_MappingElement_type"), MappingModelPackage.Literals.MAPPING_ELEMENT__DESCRIPTION, true, false, false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
 	}
 
 	/**
 	 * This adds a property descriptor for the Target Element feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addTargetElementPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_MappingElement_targetElement_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_MappingElement_targetElement_feature", "_UI_MappingElement_type"),
-				 MappingModelPackage.Literals.MAPPING_ELEMENT__TARGET_ELEMENT,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(), getResourceLocator(), getString("_UI_MappingElement_targetElement_feature"), getString("_UI_PropertyDescriptor_description", "_UI_MappingElement_targetElement_feature", "_UI_MappingElement_type"), MappingModelPackage.Literals.MAPPING_ELEMENT__TARGET_ELEMENT, true, false, true, null, null, null));
 	}
 
 	/**
 	 * This adds a property descriptor for the Source Qualifier feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addSourceQualifierPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_MappingElement_sourceQualifier_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_MappingElement_sourceQualifier_feature", "_UI_MappingElement_type"),
-				 MappingModelPackage.Literals.MAPPING_ELEMENT__SOURCE_QUALIFIER,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(), getResourceLocator(), getString("_UI_MappingElement_sourceQualifier_feature"), getString("_UI_PropertyDescriptor_description", "_UI_MappingElement_sourceQualifier_feature", "_UI_MappingElement_type"), MappingModelPackage.Literals.MAPPING_ELEMENT__SOURCE_QUALIFIER, true, false, false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
 	}
 
 	/**
@@ -142,11 +103,12 @@ public class MappingElementItemProvider
 	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
-		if (childrenFeatures == null) {
+		if(childrenFeatures == null) {
 			super.getChildrenFeatures(object);
 			childrenFeatures.add(MappingModelPackage.Literals.MAPPING_ELEMENT__ATTRIBUTES);
 		}
@@ -156,6 +118,7 @@ public class MappingElementItemProvider
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -170,6 +133,7 @@ public class MappingElementItemProvider
 	 * This returns MappingElement.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -181,14 +145,14 @@ public class MappingElementItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * 
+	 * @generated not
 	 */
 	@Override
 	public String getText(Object object) {
-		String label = ((MappingElement)object).getDescription();
-		return label == null || label.length() == 0 ?
-			getString("_UI_MappingElement_type") :
-			getString("_UI_MappingElement_type") + " " + label;
+		MappingElement elementMapping = (MappingElement)object;
+		String label = elementMapping.getDescription();
+		return label == null || label.length() == 0 ? getString("_UI_ElementMapping_type") : elementMapping.getDescription() + "-->" + elementMapping.getTargetElement().getName();
 	}
 
 	/**
@@ -196,20 +160,21 @@ public class MappingElementItemProvider
 	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public void notifyChanged(Notification notification) {
 		updateChildren(notification);
 
-		switch (notification.getFeatureID(MappingElement.class)) {
-			case MappingModelPackage.MAPPING_ELEMENT__DESCRIPTION:
-			case MappingModelPackage.MAPPING_ELEMENT__SOURCE_QUALIFIER:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
-				return;
-			case MappingModelPackage.MAPPING_ELEMENT__ATTRIBUTES:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-				return;
+		switch(notification.getFeatureID(MappingElement.class)) {
+		case MappingModelPackage.MAPPING_ELEMENT__DESCRIPTION:
+		case MappingModelPackage.MAPPING_ELEMENT__SOURCE_QUALIFIER:
+			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+			return;
+		case MappingModelPackage.MAPPING_ELEMENT__ATTRIBUTES:
+			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+			return;
 		}
 		super.notifyChanged(notification);
 	}
@@ -219,22 +184,21 @@ public class MappingElementItemProvider
 	 * that can be created under this object.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
 
-		newChildDescriptors.add
-			(createChildParameter
-				(MappingModelPackage.Literals.MAPPING_ELEMENT__ATTRIBUTES,
-				 MappingModelFactory.eINSTANCE.createMappingAttribute()));
+		newChildDescriptors.add(createChildParameter(MappingModelPackage.Literals.MAPPING_ELEMENT__ATTRIBUTES, MappingModelFactory.eINSTANCE.createMappingAttribute()));
 	}
 
 	/**
 	 * Return the resource locator for this item provider's resources.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
