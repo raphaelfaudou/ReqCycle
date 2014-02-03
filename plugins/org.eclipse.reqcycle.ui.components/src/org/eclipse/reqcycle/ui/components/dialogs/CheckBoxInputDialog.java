e62ec81f41cc76f2ee55e2e2c4ef97dfbe881a32
raphael Faudou <raphael.faudou@samares-engineering.com>
2014-02-03 21:46:14 +0100

    changed prefix from org.eclipse.reqcycle to org.polarsys.reqcycle

--- plugins/org.eclipse.reqcycle.ui.components/src/org/eclipse/reqcycle/ui/components/dialogs/CheckBoxInputDialog.java
+++ plugins/org.eclipse.reqcycle.ui.components/src/org/eclipse/reqcycle/ui/components/dialogs/CheckBoxInputDialog.java
@@ -1,16 +1,13 @@
-/*****************************************************************************
- * Copyright (c) 2013 AtoS.
- *
- *    
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *  Papa Issa DIAKHATE (AtoS) papa-issa.diakhate@atos.net - Initial API and implementation
- *
- *****************************************************************************/
+/*******************************************************************************
+ *  * Copyright (c) 2013-2014 AtoS.
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the Eclipse Public License v1.0
+ *  * which accompanies this distribution, and is available at
+ *  * http://www.eclipse.org/legal/epl-v10.html
+ *  *
+ *  * Contributors:
+ *  *    {INITIAL AUTHOR} - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.reqcycle.ui.components.dialogs;
 
 import java.util.ArrayList;
